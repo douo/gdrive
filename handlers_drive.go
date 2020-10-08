fe2a1b08f1013e307450a646dd5fbcece9c5ca96
tiou <dourokinga@gmail.com>
2020-10-08 17:56:04 +0800

    add sercet

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
-const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+const ClientId = "273215293094-aj6gbgoi66sfrbnis9thjk54sm9ttr72.apps.googleusercontent.com"
+const ClientSecret = "IdX2nfF6hNCbKqri0k1cCflI"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
